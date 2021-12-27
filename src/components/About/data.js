e4e290d9b0db1de2b2a798ca81efa2ecbd9a801d
Gustavo Conde <guoconde@pop-os.localdomain>
2021-12-27 16:21:50 -0300

    Remocao fotos duplicadas

--- src/components/About/data.js
+++ src/components/About/data.js
@@ -8,10 +8,10 @@ import Brasil from '../../assets/images/brasil.jpg'
 
 export const Data = [
     {text: "I'm Gustavo, 35 years old, married. I live in Brazil.", img: `${Me}`},
-    {text: "I'm very focused on what I'm doing!", img: `${Me}`},
-    {text: "I'm passionate about solving problems!", img: `${Me}`},
+    {text: "I'm very focused on what I'm doing!", img: ``},
+    {text: "I'm passionate about solving problems!", img: ``},
     {text: "Me and my wife love to travel!", img: `${Travel}`},
-    {text: "I'm a full-stack student at DRIVEN!", img: `${Me}`},
+    {text: "I'm a full-stack student at DRIVEN!", img: ``},
     {text: "I'm pet friendly!", img: `${PetFriendly}`},
     {text: "I'm Brasilian!", img: `${Brasil}`},
     {text: "I'm great cook.", img: `${Cook}`},
