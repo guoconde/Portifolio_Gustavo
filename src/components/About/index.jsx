7b418d0ee7e8969d057b2cb10d4b56d1766f7a77
Gustavo Conde <guoconde@pop-os.localdomain>
2021-12-29 12:10:56 -0300

    Mudando layout About

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -29,7 +29,9 @@ export default function About() {
                 <ion-icon onClick={previusImg} name="chevron-back-circle"></ion-icon>
                 <p>{Data[positionImg].text}</p>
                 <BoxImg>
-                    <img src={Data[positionImg].img} alt="PetFriendly" />
+                    <div>
+                        <img src={Data[positionImg].img} alt="PetFriendly" />
+                    </div>
                 </BoxImg>
                 <ion-icon onClick={nextImg} name="chevron-forward-circle"></ion-icon>
             </main>
@@ -71,19 +73,48 @@ const BoxAbout = styled.div`
 
         margin: 0 15px;
     }
-    `
+`
+
 const BoxImg = styled.div`
     width: 350px;
     height: 420px;
-    
-    border-style: solid;
-    border-width: 10px;
-    
-    border: double 5px transparent;
-    background-image: linear-gradient(white, white), linear-gradient(45deg, rgba(238,155,0,1) 0%, rgba(0,95,115,1) 100%);
-    border-radius: 0 180px 0 300px;
-    background-origin: border-box;
-    background-clip: content-box, border-box;
 
-    overflow: hidden;
+    border-radius: 5px;
+
+    background-color: rgba(233, 216, 166, 0.4);
+
+    position: relative;
+    
+    /* border-style: solid; */
+    /* border-width: 10px; */
+    
+    /* border: double 5px transparent; */
+    /* background-image: linear-gradient(white, white), linear-gradient(45deg, rgba(238,155,0,1) 0%, rgba(0,95,115,1) 100%); */
+    /* border-radius: 0 180px 0 300px; */
+    /* background-origin: border-box; */
+    /* background-clip: content-box, border-box; */
+
+    
+    div{
+        width: 300px;
+        height: 300px;
+        
+        border: 10px solid #001219;
+
+        border-radius: 51%;
+        overflow: hidden;
+
+        position: absolute;
+        top: 30px;
+        left: -20px;
+
+        display: flex;
+        /* justify-content: center; */
+        /* align-items: center; */
+
+        img {
+            width: 300px;
+            /* height: 100%; */
+        }
+    }
 `
